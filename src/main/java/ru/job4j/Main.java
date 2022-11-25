5671c4923767ff83fe6501da3a7a3d1db6a469c1
Vladislav1992 <kuryanov.vlad@yandex.ru>
2022-11-25 23:53:54 +0300

    6. Checkstyle [#4123] 0. Что такое переменная. [#3610]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,9 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Привет мир");
-
-        int age = 18;
-        float length = 78.0332f;
-        boolean baby = true;
     }
 }
