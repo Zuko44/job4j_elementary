ac0732875a2375645f5f04779ea2899c70ce7009
Vladislav1992 <kuryanov.vlad@yandex.ru>
2022-11-26 14:11:37 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,7 +2,9 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println(1 * 1);
+        System.out.print(1 * 1);
+        System.out.println();
+        // вывод выше добавлен чтобы быстрее сделать commit
         System.out.println(1 * 2);
         System.out.println(1 * 3);
         System.out.println(1 * 4);
