35f0291a28d62bd6f11bee7f29ca63d48e1e8073
Vladislav1992 <kuryanov.vlad@yandex.ru>
2022-11-27 20:43:42 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ConverterTest {
 
     @Test
-    public void whenConvert140RblThen2dotEuro() {
+    public void whenConvert140RblThen2Euro() {
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
@@ -15,7 +15,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert180RblThen4dot3Dlr() {
+    public void whenConvert180RblThen3Dlr() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
