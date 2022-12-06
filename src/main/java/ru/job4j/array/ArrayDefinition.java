8b6ede8bc8726670a371dbb85f47c96896b1307b
Vladislav1992 <kuryanov.vlad@yandex.ru>
2022-12-07 01:05:59 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Henri René Albert Guy de Maupassant";
+        names[3] = "Marie-Joseph Paul Yves Roch Gilbert du Motier, Marquis de La Fayette";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
